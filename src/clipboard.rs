e93a3f733b4e1bdf179ea6df8bddfc03650bdc56
fzdwx <likelovec@gmail.com>
2023-02-13 19:10:39 +0800

    feat: linux(x11) support read selection

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,4 +1,41 @@
+use std::time::Duration;
+use x11_clipboard::Clipboard;
+
+#[cfg(target_os = "linux")]
 pub fn read_text() -> Result<String, String> {
+    let clipboard = Clipboard::new().unwrap();
+    if let Ok(curr) = clipboard.load(
+        clipboard.getter.atoms.primary,
+        clipboard.getter.atoms.utf8_string,
+        clipboard.getter.atoms.property,
+        Duration::from_millis(100),
+    ) {
+        let curr = String::from_utf8_lossy(&curr)
+            .trim_matches('\u{0}')
+            .trim();
+        if !curr.is_empty() {
+            Ok(curr.to_owned())
+        } else {
+            read_text_cross()
+        }
+    } else {
+        read_text_cross()
+    }
+}
+
+
+#[cfg(target_os = "macos")]
+pub fn read_text() -> Result<String, String> {
+    read_text_cross()
+}
+
+#[cfg(target_os = "windows")]
+pub fn read_text() -> Result<String, String> {
+    read_text_cross()
+}
+
+
+fn read_text_cross() -> Result<String, String> {
     let mut clipboard = arboard::Clipboard::new().unwrap();
     clipboard.get_text().map_err(|err| err.to_string())
 }
