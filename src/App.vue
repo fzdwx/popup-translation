5a56e028481c2a281635a16b70e9befbd9596cc9
fzdwx <likelovec@gmail.com>
2023-04-07 20:00:57 +0800

    translation and setting show

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
 import Setting from '@/components/Setting/index'
 import Nav from '@/components/Nav/index'
 import Translation from '@/components/Translation/index'
+
+const { toogleSetting } = useConfigState()
+const { toogle, showSetting } = toogleSetting()
 </script>
 <template>
   <div class="app">
     <Nav />
-    <Translation />
-    <Setting />
+    <Translation v-if="!showSetting()" />
+    <Setting v-if="showSetting()" />
   </div>
 </template>
