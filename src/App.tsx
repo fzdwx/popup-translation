3f2b1e2e2341c3a17b2b25ee4bd178e123c67946
fzdwx <likelovec@gmail.com>
2023-05-10 15:36:44 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function () {
   return (
-    <div className=" text-emerald-500 text-2xl">
+    <div className="text-5xl text-green-600/50">
       hello world
     </div>
   )
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,9 @@
-import { Slot } from '@radix-ui/react-slot'
-import type { VariantProps } from 'class-variance-authority'
-import { cva } from 'class-variance-authority'
+import {Slot} from '@radix-ui/react-slot'
+import type {VariantProps} from 'class-variance-authority'
+import {cva} from 'class-variance-authority'
 import * as React from 'react'
 
-import { cn } from '@/lib/utils'
+import {cn} from '@/lib/utils'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
@@ -35,16 +35,26 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({
+     className,
+     variant,
+     size,
+     asChild = false,
+     ...props
+   }, ref) => {
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({
+          variant,
+          size,
+          className
+        }))}
         ref={ref}
         {...props}
       />
@@ -53,4 +63,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = 'Button'
 
-export { Button, buttonVariants }
+export {Button, buttonVariants}
